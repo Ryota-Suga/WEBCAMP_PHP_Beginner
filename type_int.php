b8c1b00946f202d4660c93e347638443f2532f9a
ryotasuga <ryotasuga@example.com>
2022-04-18 08:17:55 +0000

    型関連の学習用コードを書いた

---
+++ type_int.php
@@ -0,0 +1,13 @@
+<?php
+//整数型の確認
+$i=123;
+var_dump($i);
+//整数値の最大/最小値の確認
+var_dump(PHP_INT_MAX);
+var_dump(PHP_INT_MIN);
+//最大値を超えるとどうなるか
+$over_max=PHP_INT_MAX+1;
+var_dump($over_max);
+//最小値を超えるとどうなるか
+$under_min=PHP_INT_MIN-1;
+var_dump($under_min);
